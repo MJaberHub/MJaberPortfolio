d85ab89f1f9806f46cd96ae2f0042afeb183f979
Mohamad Jaber <ing.mohamad.jaber@hotmail.com>
2025-04-28 22:43:10 +0300

    enhancing cicd to invalidate cloudfront + test scripts

--- src/app/contact/contact.component.spec.ts
+++ src/app/contact/contact.component.spec.ts
@@ -1,13 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ContactComponent } from './contact.component';
+import { EmailService } from '../services/email.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { configureTestModule } from '../test-helper';
 
 describe('ContactComponent', () => {
   let component: ContactComponent;
   let fixture: ComponentFixture<ContactComponent>;
+  let emailService: EmailService;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
+      providers: [
+        EmailService
+      ]
+    });
     configureTestModule(ContactComponent);
+    
+    emailService = TestBed.inject(EmailService);
     fixture = TestBed.createComponent(ContactComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
