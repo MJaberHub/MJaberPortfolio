afb9676b8c9cd89ee96d27a70c2384d031ad223f
Mohamad Jaber <ing.mohamad.jaber@hotmail.com>
2025-05-03 23:19:08 +0300

    indexing the website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  constructor(private translate: TranslateService) {
+export class AppComponent implements OnInit {
+  constructor(
+    private translate: TranslateService,
+    private renderer: Renderer2,
+    private router: Router
+  ) {
     translate.addLangs(['en', 'fr']);
     translate.setDefaultLang('en');
     
     const browserLang = translate.getBrowserLang();
     translate.use(browserLang?.match(/en|fr/) ? browserLang : 'en');
+  }
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        const canonicalUrl = environment.domainUrl + this.router.url.split('?')[0];
+        let link: HTMLLinkElement | null = document.querySelector('link[rel="canonical"]');
+        if (!link) {
+          link = this.renderer.createElement('link');
+          link!.setAttribute('rel', 'canonical');
+          this.renderer.appendChild(document.head, link);
+        }
+        link!.setAttribute('href', canonicalUrl);
+      }
+    });
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    apiSendEmailUrl: 'https://tmgbqfzerb.execute-api.us-east-1.amazonaws.com/prod/send-email'
+    apiSendEmailUrl: 'https://tmgbqfzerb.execute-api.us-east-1.amazonaws.com/prod/send-email',
+    domainUrl: 'https://mohamad-jaber.online'
   };
